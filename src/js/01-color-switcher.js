18fdbfbe1762d0130e9d86f3b160e25fc1f61dc1
Rew <irinavakulenko2000@gmail.com>
2023-03-21 21:55:48 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const body = document.querySelector('body');
+const startBtnColor = document.querySelector('button[data-start]');
+// console.log(startBtnColor);
+const stopBtnColor = document.querySelector('button[data-stop]');
+// console.log(stopBtnColor);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+startBtnColor.addEventListener('click', handleStartColor);
+stopBtnColor.addEventListener('click', handleStopColor);
+
+function handleStartColor() {
+
+    timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        body.style.backgroundColor = `${randomColor}`;
+    }, 1000);
+
+    startBtnColor.setAttribute('disabled', '');
+
+}
+
+function handleStopColor() {
+    clearInterval(timerId);
+    startBtnColor.removeAttribute('disabled', '');
+}
