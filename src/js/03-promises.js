f4d7e64ed3621a05437594f2319c4850702f13cb
Rew <irinavakulenko2000@gmail.com>
2023-03-25 01:41:50 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,59 +1,95 @@
+const refs = {
+  delayInput: document.querySelector('input[name="delay"]'),
+  stepInput: document.querySelector('input[name = "step"]'),
+  amountInput: document.querySelector('input[name = "amount"]'),
+  formSubmit: document.querySelector('.form'),
+}
+
+// const amount = refs.amountInput.addEventListener('inut', handlAmount);
+//  const step = refs.stepInput.addEventListener('input', handleStep);
+//  const delayFirst = refs.delayInput.addEventListener('input', handlDelay);
+
+const delayFirst = Number(refs.delayInput.value);
+const step = Number(refs.stepInput.value);
+const amount = Number(refs.amountInput.value);
+
 function createPromise(position, delay) {
-  let delay = firstDelay;
-  return new Promise((resolve, reject) => {
+ 
+  const promise = new Promise((resolve, reject) => {
+    
     const shouldResolve = Math.random() > 0.3;
     
     setTimeout(() => {
-            if (shouldResolve) {
-    // Fulfill
-            }
-            else {
-    // Reject
-            }
-      
-    
-        }, delay)
-
-  
+      if (shouldResolve) {
+        // Fulfill  
+        resolve({ position, delay });
+      }
+      else {
+        // Reject
+        reject({ position, delay });
+      }
+         
+    }, delayFirst)
   })
+ 
+  promise.then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+ return promise;
 
 }
 
-const refs = {
-  delayInput: document.querySelector('[name="delay"]'),
-  stepInput: document.querySelector('[name = "step"]'),
-  amountInput: document.querySelector('[name = "amount"]'),
-  formSubmit: document.querySelector('button'),
-}
 
-let firstDelay = null;
-let delayStep = null;
-let amount = null;
+
+createPromise(amount, delayFirst);
+
+ 
+
 
 console.log(refs.delayInput, refs.stepInput, refs.amountInput);
 
-refs.delayInput.addEventListener('input', handlDelay);
-  function handlDelay (event) {
-  firstDelay = event.target.value;
-  console.log(firstDelay);
+
+refs.formSubmit.addEventListener('submit', handleCreatePromise);
+
+console.log(refs.formSubmit);
+
+function handleCreatePromise(event) {
+  event.preventDefault();
+  
+const delayFirst = Number(refs.delayInput.value);
+const step = Number(refs.stepInput.value);
+const amount = Number(refs.amountInput.value);
+  // let promises = [];
+  
+let delayNext = delayFirst;
+
+
+  for (i = 1; i <= amount; i += 1) {
+    
+  createPromise([i], delayNext[i]);
+    delayNext += step;
   }
 
-refs.stepInput.addEventListener('input', handleStep);
-function handleStep(event) {
-  delayStep = event.target.value;;
-  console.log(delayStep);
+  // console.log(promises);
+  // return promises;
 }
 
-refs.amountInput.addEventListener('inut', handlAmount);
-function handlAmount(event) {
-  amount = event.target.value;
-  console.log(amount);
-}
 
-refs.formSubmit.addEventListener('submit', createPromise);
+//   function handlAmount(event) {
+//    amount = event.target.value;
+//   }
 
 
-
+// function handleStep(event) {
+//   delayStep = event.target.value;;
+// }
+//   function handlDelay (event) {
+//   firstDelay = event.target.value;
+//     console.log(firstDelay);
+//   }  
 
 
 
