3809042aa4069403fa7a3be50a3a37a9f5b8198c
Rew <irinavakulenko2000@gmail.com>
2023-03-24 12:41:54 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,59 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+  let delay = firstDelay;
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    
+    setTimeout(() => {
+            if (shouldResolve) {
     // Fulfill
-  } else {
+            }
+            else {
     // Reject
-  }
+            }
+      
+    
+        }, delay)
+
+  
+  })
+
 }
+
+const refs = {
+  delayInput: document.querySelector('[name="delay"]'),
+  stepInput: document.querySelector('[name = "step"]'),
+  amountInput: document.querySelector('[name = "amount"]'),
+  formSubmit: document.querySelector('button'),
+}
+
+let firstDelay = null;
+let delayStep = null;
+let amount = null;
+
+console.log(refs.delayInput, refs.stepInput, refs.amountInput);
+
+refs.delayInput.addEventListener('input', handlDelay);
+  function handlDelay (event) {
+  firstDelay = event.target.value;
+  console.log(firstDelay);
+  }
+
+refs.stepInput.addEventListener('input', handleStep);
+function handleStep(event) {
+  delayStep = event.target.value;;
+  console.log(delayStep);
+}
+
+refs.amountInput.addEventListener('inut', handlAmount);
+function handlAmount(event) {
+  amount = event.target.value;
+  console.log(amount);
+}
+
+refs.formSubmit.addEventListener('submit', createPromise);
+
+
+
+
+
+
