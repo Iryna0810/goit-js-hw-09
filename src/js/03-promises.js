bbea4a590d6360b3c4dcc47186a580d2d21af872
Rew <irinavakulenko2000@gmail.com>
2023-03-26 14:06:51 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,14 +5,6 @@ const refs = {
   formSubmit: document.querySelector('.form'),
 }
 
-// const amount = refs.amountInput.addEventListener('inut', handlAmount);
-//  const step = refs.stepInput.addEventListener('input', handleStep);
-//  const delayFirst = refs.delayInput.addEventListener('input', handlDelay);
-
-const delayFirst = Number(refs.delayInput.value);
-const step = Number(refs.stepInput.value);
-const amount = Number(refs.amountInput.value);
-
 function createPromise(position, delay) {
  
   const promise = new Promise((resolve, reject) => {
@@ -29,7 +21,7 @@ function createPromise(position, delay) {
         reject({ position, delay });
       }
          
-    }, delayFirst)
+    }, delay)
   })
  
   promise.then(({ position, delay }) => {
@@ -39,57 +31,25 @@ function createPromise(position, delay) {
       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     });
  return promise;
-
 }
-
-
-
-createPromise(amount, delayFirst);
-
- 
-
-
-console.log(refs.delayInput, refs.stepInput, refs.amountInput);
-
 
 refs.formSubmit.addEventListener('submit', handleCreatePromise);
 
-console.log(refs.formSubmit);
-
 function handleCreatePromise(event) {
-  event.preventDefault();
+event.preventDefault();
+
+  const delayFirst = Number(refs.delayInput.value);
+  const amount = Number(refs.amountInput.value);
   
-const delayFirst = Number(refs.delayInput.value);
-const step = Number(refs.stepInput.value);
-const amount = Number(refs.amountInput.value);
-  // let promises = [];
-  
-let delayNext = delayFirst;
-
-
-  for (i = 1; i <= amount; i += 1) {
-    
-  createPromise([i], delayNext[i]);
-    delayNext += step;
-  }
-
-  // console.log(promises);
-  // return promises;
+  setTimeout(() => {
+    const step = Number(refs.stepInput.value);
+    let delayNext = 0;
+    for (i = 1; i <= amount; i += 1) {
+      delayNext += step;
+      createPromise(i, delayNext);
+      }
+  }, delayFirst) 
 }
-
-
-//   function handlAmount(event) {
-//    amount = event.target.value;
-//   }
-
-
-// function handleStep(event) {
-//   delayStep = event.target.value;;
-// }
-//   function handlDelay (event) {
-//   firstDelay = event.target.value;
-//     console.log(firstDelay);
-//   }  
 
 
 
